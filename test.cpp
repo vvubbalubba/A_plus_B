4ebd82bae38639281808fcb358fb617fbd141c06
Anna <fomina.av@phystech.edu>
2020-04-13 18:35:57 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,8 +2,13 @@
 #include "sum.cpp"
 #include <gtest/gtest.h>
 
-TEST(sum, PositiveNos) { 
+TEST(sum, test1) { 
     ASSERT_EQ(3, sum(1, 2));
+    ASSERT_EQ(4, sum(2, 2));
+}
+
+TEST(sum, test2) {
+    ASSERT_EQ(5, sum(2, 2));
 }
 
 int main(int argc, char **argv) {
